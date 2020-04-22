ffa0fb35f631197166b093ddad84dfa39ac4a66b
Luis Moscoso <Lm239292@gmail.com>
2020-04-22 02:01:13 -0400

    test

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {
 
-   app.use(express.static(path.join(__dirname, 'client/build')));  
-
    app.get('*', (req, res) => {res.sendfile(path.join(__dirname = 'client/build/index.html'));})
 
 }
